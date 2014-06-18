86a322513c8cb98170e2697e09e7c77cb3ed20c7
Conway <gongwei.chen@gmail.com>
2014-06-18 18:13:36 +0800

    update

--- src/main/resources/table.sql
+++ src/main/resources/table.sql
@@ -1,12 +1,12 @@
 create table User (
 	id number(8) primary key, 
-	user_name varchar(20), 
-	password varchar(32), 
+	user_name varchar(20) not null, 
+	password varchar(32) not null, 
 	last_login_ip varchar(15),
 	last_login_time timestamp,
 	login_times number(2),
-	disabled char(1),
-	need_change_pwd char(1),
+	disabled char(1) default 'N',
+	need_change_pwd char(1) default 'N',
 	pwd_change_date date
 );
 
@@ -24,10 +24,14 @@ create table User_Info(
 	mobile varchar(20),
 	address varchar(200),
 	remark varchar(100),
-	create_date date,
+	create_date date default sysdate,
 	update_date date
 );
 
+alter table User_Info
+  add constraint FK_User_Info_User_ID foreign key (user_id)
+  references User(id);
+  
 create table Role(
 	role_id number(8) primary key,
 	role_name varchar(30),
@@ -46,6 +50,13 @@ create table User_role(
 	role_id number(8)
 );
 
+alter table User_role
+  add constraint FK_User_role_User_ID foreign key (user_id)
+  references User(id);
+alter table User_role
+  add constraint FK_User_role_Role_ID foreign key (role_id)
+  references Role(role_id);
+  
 create table Module(
 	module_id number(10) primary key,
 	parent_id number(10),
@@ -61,3 +72,10 @@ create table Role_module(
 	role_id number(8),
 	module_id number(10)
 );
+
+alter table Role_module
+  add constraint FK_Role_module_Role_ID foreign key (role_id)
+  references Role(role_id);
+alter table Role_module
+  add constraint FK_Role_module_Role_ID foreign key (module_id)
+  references Module(module_id);
